31bed48b96905066164d7055ffab3afb10614c89
Melly <Marceliskorupski@gmail.com>
2021-03-23 15:49:18 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-import Solution
+
 
 def GenerateDataSet(n, factors):
     dictTMP = {}  # declaring a temporary dictionary
@@ -14,7 +14,8 @@ def GenerateDataSet(n, factors):
         while True:
             lenbefore = len(dictTMP)
             ''' Name is now a set which also contains the age '''
-            tmpIndiv = tuple([Names[random.randint(0, (len(Names) - 1))], random.randint(0, 100)])
+            tmpIndiv = tuple(
+                [Names[random.randint(0, (len(Names) - 1))], random.randint(0, 100), random.randint(0, 9999999)])
             # tmpIndiv = { Names[random.randint(0, (len(Names) - 1))] } # selects random name and assigns to var
 
             tmpVuln = set()  # declares a set which will contain Vulnerabilities
@@ -45,19 +46,18 @@ if __name__ == '__main__':
     Data = None  # Ditctionary of individuals to be sorted
     IncVFactors = ['Smoking', 'Drinking',
                    'HealthIssue', 'TraveledAbroad',
-                   'EssentialWorker']  # List of factors which increase to Vaunerability of an individual
+                   'EssentialWorker']  # List of factors which increase to Vulnerability of an individual
     ''' 
     IncVfactors is a lsit which contains the factors which increase the vulnerability of an individual    
     [ 'Smoking' , 'Drinking' , ' HealthIssue ' ]
-    Individuals Contain a name and factors relating to their vulnerability to COVID
-    THE AGE WILL ALWAYS BE THE FIRST VALUE
-    {  'Marceli' : { 18 , Smoking } , 'Name' : { Age , Issues... }}
+    Individuals Contain a name , age , ID number and factors relating to their vulnerability to COVID
+    final dict will look as such:
+    {  (name, age, ID number):{ Vulnerabilities } 
 
     '''
     amount = int(input("please enter set amount"))
     Data = GenerateDataSet(amount, IncVFactors)
     print(Data)
-    print(len(Data))
     print("would you like to write the generated data to a file? (y/n)")
     if input() == 'y' or 'Y':
         WriteData2FIle()
